c894a3552dc8cc35d8fce8ff435081fff61dfd06
Doug Jones <DSJR1284@GMAIL.COM>
2021-09-07 19:41:55 -0400

    fixed params issue in users, questions, and answers controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.fetch(:user, {})
+      params.require(:user).permit(:username, :password, :avatar)
     end
 end
